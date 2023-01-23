e695f6fb56fd339b53699bc9205f9a9dbc7d5a4a
olvneto <olvneto@yahoo.com.br>
2023-01-23 12:46:23 -0300

    Até aula 10.30

--- src/main/java/com/algaworks/ecommerce/model/PagamentoBoleto.java
+++ src/main/java/com/algaworks/ecommerce/model/PagamentoBoleto.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -14,6 +15,9 @@ import javax.persistence.Entity;
 //@Table(name = "pagamento_boleto")
 public class PagamentoBoleto extends Pagamento {
 
-  @Column(name = "codigo_barras", length = 100)
-  private String codigoBarras;
+    @Column(name = "codigo_barras", length = 100)
+    private String codigoBarras;
+
+    @Column(name = "data_vencimento")
+    private LocalDate dataVencimento;
 }
